2e6243981d9974ac39c7f0a5dca764c2615cbc48
Kee0304 <kee0304@gmail.com>
2024-08-22 17:51:05 +0900

    껍데기 작업중

--- src/pages/room/roomPage.tsx
+++ src/pages/room/roomPage.tsx
@@ -1,7 +1,34 @@
-import { useParams } from 'react-router-dom'
+import { useEffect, useRef, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { CodeSnapshot } from '../../model/CodeSnapshot';
+import { CodeSnapshotRepository } from '../../services/CodeSnapshotRepository';
+import { CommentRepository } from '../../services/CommentRepository';
+import { Comment } from '../../model/Comment';
 
 const RoomPage = () => {
-  const params = useParams();
+  // 파라미터
+  const params:{roomId:string} = useParams<{ roomId: string }>();;
+  const [snapshots, setSnapshots] = useState<CodeSnapshot[]>([]);
+  const [comments, setComments] = useState<Comment[]>([]);
+  const month = useRef<number>(new Date().getMonth());
+  const commentPage = useRef<number>(0);
+
+   async function getData():Promise<void> {
+    setSnapshots([...await CodeSnapshotRepository.getInstance().getSnapshots(month.current)]);
+    setComments([...await CommentRepository.getInstance().getComments(commentPage.current)]);
+  }
+
+  useEffect(() => {
+    getData();
+  },[])
+
+  useEffect(() => {
+    console.log(snapshots);
+  },[snapshots])
+
+  useEffect(() => {
+    console.log(comments);
+  },[comments])
 
   return (
     <div>
