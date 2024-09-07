8a0fb5c82cd57fa4611100865a610116bc606a0f
Kee0304 <kee0304@gmail.com>
2024-09-07 12:35:40 +0900

    feat/codesnapshot : 기능 연동 테스트

--- src/model/RoomInfo.ts
+++ src/model/RoomInfo.ts
@@ -1,12 +1,18 @@
 export class RoomInfo {
+    private id?:number;
+    private uuid?: string;
     private name:string;
     private description:string;
-    private content:string;
+    private content?:string;
 
-    public constructor(name:string, description: string, content:string) {
+    public constructor(name:string, description: string, content?:string,
+        id?:number, uuid?:string
+    ) {
         this.name = name;
         this.description = description;
         this.content = content;
+        this.id = id;
+        this.uuid = uuid;
     }
 
     static fromJson(data:any) {
@@ -17,7 +23,45 @@ export class RoomInfo {
         )
     }
 
+    static fromListJson(data:any) {
+        return new RoomInfo(
+            data.name,
+            data.description,
+            undefined,
+            data.id,
+            data.uuid
+        )
+    }
+
+    public getId():number {
+        if (this.id) {
+            return this.id;
+        } else {
+            return -1;
+        }
+    }
+
     public getContent():string {
-        return this.content;
+        if (this.content) {
+            return this.content;
+        } else {
+            return "";
+        }
+    }
+
+    public getUuid():string {
+        if (this.uuid) {
+            return this.uuid;
+        } else {
+            return "";
+        }
+    }
+
+    public getName():string {
+        return this.name;
+    }
+
+    public getDescription():string {
+        return this.description;
     }
 }
--- src/pages/manage/managePage.tsx
+++ src/pages/manage/managePage.tsx
@@ -1,36 +1,57 @@
 import { AxiosResponse } from "axios"
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import axi from "../../utils/axios/Axios";
-import Cookies from 'js-cookie';
+import { RoomInfo } from "../../model/RoomInfo";
+import Sidebar from "../../components/SideBar";
+import { NavigateFunction, useNavigate } from "react-router-dom";
+
 
 const ManagePage = () => {
-
-  const getRooms = async() => {
-    console.log(axi.defaults.withCredentials);
-    const response:AxiosResponse = await axi.get('rooms');
-    console.log(response);
-    return response.data;
-  }
-
-  const getCookie = () => {
-    const jsessionId = Cookies.get("JSESSIONID");
-    console.log(jsessionId);
-  }
+  const navigator:NavigateFunction = useNavigate();
+  const [rooms, setRooms] = useState<RoomInfo[]>([]);
   
+  const getRooms = async():Promise<void> => {
+    const response:AxiosResponse = await axi.get('rooms');
+    setRooms(response.data.map((room:unknown) => RoomInfo.fromListJson(room)));
+  }
+
+  const goToRoom = (roomUuid:string):void => {
+    navigator(`room/${roomUuid}`);
+  }
+
   useEffect(() => {
-    getCookie();
     getRooms();
   },[])
 
   return (
-    <div>
-      매니지 페이지
-      <button onClick = {() => {getRooms()}}>
-        쿠키 테스트
-      </button>
-    </div>
+    <div className="flex w-full">
+      {/* Sidebar */}
+      <Sidebar rooms={rooms} goToRoom = {goToRoom} />
 
-  )
+      <div className="w-full flex flex-col">
+        {/* Header */}
+        <div className=" bg-[#282C34] items-center h-16 w-fulljustify-between p-5">
+          <h1 className="text-xl font-bold text-white">최근 항목</h1>
+        </div>
+        {/* Main Content */}
+        <div className="flex-1 bg-[#212121] p-6">
+          {/* Cards */}
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+            {rooms?.map((room) => (
+              <div 
+              key={room.getId()} 
+              className="bg-white p-4 shadow-md rounded-lg cursor-pointer"
+              onClick={() => {goToRoom(room.getUuid())}}
+              >
+                <h2 className="text-lg font-semibold">{room.getName()}</h2>
+                <p className="text-gray-600">{room.getDescription()}</p>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default ManagePage
+export default ManagePage;
