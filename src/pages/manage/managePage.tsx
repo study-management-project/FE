2c55b688842b85df4d067519a8d744736566e457
Kee0304 <kee0304@gmail.com>
2024-10-02 17:42:05 +0900

    filter modi

--- src/pages/manage/managePage.tsx
+++ src/pages/manage/managePage.tsx
@@ -69,7 +69,7 @@ const ManagePage = () => {
             {/* Cards */}
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
               {rooms?.map((room) => (
-                <RoomCard room={room} goToRoom={goToRoom} />
+                <RoomCard room={room} goToRoom={goToRoom} id={room.getId()} />
               ))}
             </div>
           </div>
